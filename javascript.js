82fc032d1d19ff3875c64117a2ab9d71b7c26aec
RAmy <2218520@talnet.nl>
2024-12-08 17:16:15 +0100

    Add files via upload
    heb basis dingen geschreven

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.querySelector('#search-btn');
+    searchButton.addEventListener('click', () => {
+      alert('Zoekfunctionaliteit komt binnenkort!');
+    });
+  });
+  
